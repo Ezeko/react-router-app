2c09df927341e9b00514acb802d973c2bd2e07c8
Ezekiel ADEJOBI <ezeko2017@gmail.com>
2020-10-14 17:45:43 +0100

    update

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,23 +1,29 @@
 import React, {useState} from 'react'
 import { NavLink, Prompt } from 'react-router-dom'
 
-const Index = () => {
-    const [inputValue, setInputValue] = useState('');
+const Index = (props) => {
+    console.log(props)
+    const [isEmpty, setIsEmpty] = useState(false);
+    const[something, setSomething] = useState('');
     const submitHandler = (e) => {
         e.preventDefault();
         e.target.reset();
-        setInputValue('');
+
+        setIsEmpty(false)
     }
     return(
         <div className="App">
+        {console.log(something)}
             <nav className="black dark">
                 <NavLink to='/home' > Go to home page</NavLink>
             </nav>
-            <Prompt when={inputValue} message='Are you sure you want to leave this page?' />
+            <Prompt when={isEmpty} message='Are you sure you want to leave this page?' />
             
             <div className='container'>
                 <form onSubmit={submitHandler}>
-                    <input type='text' placeholder='type here to test transition blocker' onChange={(text)=> setInputValue(text)}/>
+                    <input type='text' placeholder='type here to test transition blocker' onChange = {(text)=>{setIsEmpty(true)
+                    setSomething(text.target.value)
+                    }}/>
                 </form>
 
             </div>
