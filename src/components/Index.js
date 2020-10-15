34f6f67f23cac76b5ed5de85648223bf345bc3c6
Ezekiel ADEJOBI <ezeko2017@gmail.com>
2020-10-15 10:48:51 +0100

    restructure state with hook

--- src/components/Index.js
+++ src/components/Index.js
@@ -3,27 +3,25 @@ import { NavLink, Prompt } from 'react-router-dom'
 
 const Index = (props) => {
     console.log(props)
-    const [isEmpty, setIsEmpty] = useState(false);
-    const[something, setSomething] = useState('');
+    const [state, setState] = useState({isEmpty: false, something: ''});
     const submitHandler = (e) => {
         e.preventDefault();
         e.target.reset();
 
-        setIsEmpty(false)
+        setState({isEmpty: false})
     }
     return(
         <div className="App">
-        {console.log(something)}
+        {console.log(state.something)}
             <nav className="black dark">
                 <NavLink to='/home' > Go to home page</NavLink>
             </nav>
-            <Prompt when={isEmpty} message='Are you sure you want to leave this page?' />
+            <Prompt when={state.isEmpty} message='Are you sure you want to leave this page?' />
             
             <div className='container'>
                 <form onSubmit={submitHandler}>
-                    <input type='text' placeholder='type here to test transition blocker' onChange = {(text)=>{setIsEmpty(true)
-                    setSomething(text.target.value)
-                    }}/>
+                    <input type='text' placeholder='type here to test transition blocker' onChange = {(text)=>setState({isEmpty: true, something: text.target.value})
+                    }/>
                 </form>
 
             </div>
